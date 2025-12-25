7e8aeb459c798366183c95110c93c8349c28588d
FreezB11 <yashrajrao4@gmail.com>
2025-12-26 01:58:07 +0530

    v1.5 tbh, preemptive works; timer also;

--- src/scheduler/scheduler.c
+++ src/scheduler/scheduler.c
@@ -1,14 +1,17 @@
 ///@file:scheduler.c
+#define _GNU_SOURCE
 #include "../thread/thread.h"
 #include "../thread/stack.h"
 #include "../context/context.h"
+#include <signal.h>
+#include <sys/time.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
 
 static uthr_thread_t *current = NULL;
 static uthr_thread_t *run_queue = NULL;
 static context_t scheduler_ctx;
-static int thread_count = 0;
 
 static void enqueue(uthr_thread_t *t){
     t->next = NULL;
@@ -32,6 +35,41 @@ static uthr_thread_t *dequeue(){
     run_queue = run_queue->next;
     return t;
 }
+
+/*
+we will use alarm_mask to block SIGALRM during scheduling
+*/
+static sigset_t alarm_mask;
+// preemption pt
+static void timer_handler(int sig){
+    (void)sig;
+    if(!current || current->state != THREAD_RUNNING) return;
+
+    current->state = THREAD_READY;
+    enqueue(current);
+
+    context_switch(&current->context, &scheduler_ctx);
+}
+
+// timer-installation
+static void timer_start(){
+    struct sigaction sa;
+    sa.sa_handler = timer_handler;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_RESTART;
+
+    sigaction(SIGALRM, &sa, NULL);
+
+    struct itimerval timer;
+    timer.it_interval.tv_sec = 0;
+    timer.it_interval.tv_usec = 10000; // 10ms time slice
+    timer.it_value = timer.it_interval;
+
+    setitimer(ITIMER_REAL, &timer, NULL);
+}
+
+static int thread_count = 0;
+
 // __attribute__((noreturn))
 void uthr_exit(void){
     current->state = THREAD_DONE;
@@ -63,16 +101,63 @@ int uthr_create(void(*fn)(void *), void *arg){
     return t->id;
 }
 
+// was required in v1
+// void uthr_yield(void){
+//     current->state = THREAD_READY;
+//     enqueue(current);
+//     context_switch(&current->context, &scheduler_ctx);
+// }
+
+static void scheduler_lock(){
+    sigprocmask(SIG_BLOCK, &alarm_mask, NULL);
+}
+
+static void scheduler_unlock(){
+    sigprocmask(SIG_UNBLOCK, &alarm_mask, NULL);
+}
+// optional now
 void uthr_yield(void){
+    scheduler_lock();
     current->state = THREAD_READY;
     enqueue(current);
+    scheduler_unlock();
+
     context_switch(&current->context, &scheduler_ctx);
 }
 
-void scheduler_run(){
-    context_switch(&scheduler_ctx, &scheduler_ctx);
+static void scheduler_init(){
+    sigemptyset(&alarm_mask);
+    sigaddset(&alarm_mask, SIGALRM);
+    timer_start();
+}
 
-    while((current = dequeue()) != NULL){
+// v1
+// void scheduler_run(){
+//     context_switch(&scheduler_ctx, &scheduler_ctx);
+
+//     while((current = dequeue()) != NULL){
+//         if(current->state == THREAD_DONE){
+//             free(current->stack);
+//             free(current);
+//             continue;
+//         }
+
+//         current->state = THREAD_RUNNING;
+//         context_switch(&scheduler_ctx, &current->context);
+//     }
+// }
+
+// v1.5
+void scheduler_run(){
+    scheduler_init();
+
+    while(1){
+        scheduler_lock();
+        current = dequeue();
+        scheduler_unlock();
+
+        if(!current) break;
+
         if(current->state == THREAD_DONE){
             free(current->stack);
             free(current);
