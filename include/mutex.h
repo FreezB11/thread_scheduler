6cc7470d6ddcdfabdefbfae4a67f10e05be0ac2b
FreezB11 <yashrajrao4@gmail.com>
2026-02-01 16:11:33 +0530

    atomic mutex - closes #1

--- include/mutex.h
+++ include/mutex.h
@@ -1,9 +1,12 @@
 ///@file: mutex.h
 #pragma once
 #include "queue.h"
+#include <stdatomic.h>
 
+// we will make this atomic int since we can extend
+// this scheduler to m:n then we need atomics
 typedef struct mutex{
-    int locked;
+    _Atomic int locked;
     queue wait_queue;
 }mutex_t;
 
